fb39b07ca47b201de8726f8c49adb4167c2e3c19
BlaineLambert <flambert24@basecampcodingacademy.org>
2023-11-06 13:38:15 -0600

    done

--- app/views.py
+++ app/views.py
@@ -3,11 +3,11 @@ from django.http.response import HttpResponse
 from django.http.request import HttpRequest
 # Create your views here.
 
-def hello_view(request, name):
+def hello_view(request: HttpRequest, name: HttpRequest) -> HttpRequest:
     return HttpResponse(f"Hey, {name}!")
 
-def how_old(request, year, birthyear):
+def how_old(request: HttpRequest, year: HttpRequest, birthyear: HttpRequest) -> HttpRequest:
     return HttpResponse(f"{year - birthyear}")
 
-def order(request, burber, fries, drink):
+def order(request: HttpRequest, burber: HttpRequest, fries: HttpRequest, drink: HttpRequest) -> HttpResponse:
     return HttpResponse(f"{burber * 4.5 + drink * 1 + fries * 1.5}")
